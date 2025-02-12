d49fbebe80fc538856e7de7bda9cd4847ca586ce
Kirisan Thiruchelvam <73609371+kkpro18@users.noreply.github.com>
2025-02-12 02:18:29 +0000

    Preprocessing fixes Plotly requires ISO 8601 format, dates works for now, no missing values

--- Experiments/5_application_interface/App/pages/1_📁_Upload_Dataset.py
+++ Experiments/5_application_interface/App/pages/1_📁_Upload_Dataset.py
@@ -21,69 +21,76 @@ if uploaded_dataset is not None:
     st.success("File uploaded successfully!")
 
 if 'uploaded_dataset' in st.session_state:
-    if "confirmation_button_clicked" not in st.session_state:
+    if 'confirmation_button_clicked' not in st.session_state:
         st.session_state.confirmation_button_clicked = False
-    st.write(st.session_state['uploaded_dataset'].head())
 
-    st.write("Select Columns to Use For the Forecast")
+    if not st.session_state.confirmation_button_clicked:
+        st.write(st.session_state['uploaded_dataset'].head())
 
-    columns = list(st.session_state['uploaded_dataset'].columns)
+        st.write("Select Columns to Use For the Forecast")
 
-    # invoice date // pre-process data e.g. some may have times
-    if 'selected_date_column' not in st.session_state:
-        st.session_state.selected_date_column = 0
-    date_column = st.selectbox("Select the Column for Invoice Date", options=columns, index=st.session_state.selected_date_column, disabled=st.session_state.confirmation_button_clicked)
-    st.session_state["date_column"] = date_column
-    if date_column in columns:
-        st.session_state.selected_date_column = columns.index(date_column)
+        columns = list(st.session_state['uploaded_dataset'].columns)
 
-    # productID/ProductName // separate into products and its own sales
-    columns.remove(date_column)
-    if 'selected_product_column' not in st.session_state:
-        st.session_state.selected_product_column = 0
-    product_column = st.selectbox("Select the Column for Product ID / Name", options=columns, index=st.session_state.selected_product_column,
-                                  disabled=st.session_state.confirmation_button_clicked)
-    st.session_state["product_column"] = product_column
-    if product_column in columns:
-        st.session_state.selected_product_column = columns.index(product_column)
+        # invoice date // pre-process data e.g. some may have times
+        if 'selected_date_column' not in st.session_state:
+            st.session_state.selected_date_column = 0
+        date_column = st.selectbox("Select the Column for Invoice Date", options=columns, index=st.session_state.selected_date_column, disabled=st.session_state.confirmation_button_clicked)
+        st.session_state["date_column"] = date_column
+        if date_column in columns:
+            st.session_state.selected_date_column = columns.index(date_column)
 
-    # price
-    columns.remove(product_column)
-    if 'selected_price_column' not in st.session_state:
-        st.session_state.selected_price_column = 0
-    unit_price_column = st.selectbox("Select the Column for Unit Price", options=columns, index=st.session_state.selected_price_column,
-                                     disabled=st.session_state.confirmation_button_clicked)
+        # productID/ProductName // separate into products and its own sales
+        columns.remove(date_column)
+        if 'selected_product_column' not in st.session_state:
+            st.session_state.selected_product_column = 0
+        product_column = st.selectbox("Select the Column for Product ID / Name", options=columns, index=st.session_state.selected_product_column,
+                                      disabled=st.session_state.confirmation_button_clicked)
+        st.session_state["product_column"] = product_column
+        if product_column in columns:
+            st.session_state.selected_product_column = columns.index(product_column)
 
-    st.session_state["unit_price_column"] = unit_price_column
-    if unit_price_column in columns:
-        st.session_state.selected_price_column = columns.index(unit_price_column)
+        # price
+        columns.remove(product_column)
+        if 'selected_price_column' not in st.session_state:
+            st.session_state.selected_price_column = 0
+        unit_price_column = st.selectbox("Select the Column for Unit Price", options=columns, index=st.session_state.selected_price_column,
+                                         disabled=st.session_state.confirmation_button_clicked)
+        st.session_state["unit_price_column"] = unit_price_column
+        if unit_price_column in columns:
+            st.session_state.selected_price_column = columns.index(unit_price_column)
 
-    # Quantity Sold
-    columns.remove(unit_price_column)
+        # Quantity Sold
+        columns.remove(unit_price_column)
 
-    if 'selected_units_sold_column' not in st.session_state:
-        st.session_state.selected_units_sold_column = 0
-    units_sold_column = st.selectbox("Select the Column for Units Sold", options=columns, index=st.session_state.selected_units_sold_column,
-                                     disabled=st.session_state.confirmation_button_clicked)
-    st.session_state["units_sold_column"] = units_sold_column
-    st.session_state.selected_units_sold_column = columns.index(units_sold_column)
+        if 'selected_units_sold_column' not in st.session_state:
+            st.session_state.selected_units_sold_column = 0
+        units_sold_column = st.selectbox("Select the Column for Units Sold", options=columns, index=st.session_state.selected_units_sold_column,
+                                         disabled=st.session_state.confirmation_button_clicked)
+        st.session_state["units_sold_column"] = units_sold_column
+        st.session_state.selected_units_sold_column = columns.index(units_sold_column)
 
-    confirmation_button_clicked = st.button("Confirm Selected Options")
-    if confirmation_button_clicked:
-        st.session_state.confirmation_button_clicked = confirmation_button_clicked
-        st.rerun()
+        regions = ["UK", "USA"]
+        if 'selected_region' not in st.session_state:
+            st.session_state.selected_region = 0
+        selected_region = st.session_state.selected_region
+        region = st.radio(label="Enter the region where your store is based?", options=regions, index=st.session_state.selected_region, disabled=st.session_state.confirmation_button_clicked)
+        st.session_state["region"] = region
+        st.session_state.selected_region = regions.index(region)
+        st.markdown(f"You Selected {st.session_state.region}.")
 
-    st.write("Dataset with the selected columns:")
-    st.session_state.uploaded_dataset = st.session_state.uploaded_dataset[
-        [date_column, product_column, unit_price_column, units_sold_column]]
-    st.write(st.session_state.uploaded_dataset.head())
+        confirmation_button_clicked = st.button("Confirm Selected Options", disabled=st.session_state.confirmation_button_clicked)
+        if confirmation_button_clicked:
+            st.session_state.uploaded_dataset = st.session_state.uploaded_dataset[[date_column, product_column, unit_price_column, units_sold_column]]
+            st.session_state.confirmation_button_clicked = True
+    else:
+        st.success("Selection Complete!")
+        st.write("Dataset with the selected columns: ")
+        st.write("Invoice Date Column: ", st.session_state.date_column)
+        st.write("Product Info Column: ", st.session_state.product_column)
+        st.write("Unit Price: ", st.session_state.unit_price_column)
+        st.write("Units Sold Column: ", st.session_state.units_sold_column)
+        st.write("Region: ", st.session_state.region)
 
-    options = ["UK", "USA"]
-    if 'selected_region' not in st.session_state:
-        st.session_state.selected_region = 0
-    selected_region = st.segmented_control("Enter the region where your store is based?", options=options,
-                                           selection_mode="single", default=options[0])
-    st.session_state["selected_region"] = selected_region
-    st.markdown(f"You Selected {st.session_state.selected_region}.")
+        # st.switch_page("pages/2_🔎_Explore_Data.py")
 
     st.page_link("pages/2_🔎_Explore_Data.py", label="👈 Next Stage: Visualise The Dataset", icon="🔎")
--- Experiments/5_application_interface/App/pages/2_🔎_Explore_Data.py
+++ Experiments/5_application_interface/App/pages/2_🔎_Explore_Data.py
@@ -24,16 +24,18 @@ tqdm.pandas() # for progress_apply
 if 'uploaded_dataset' in st.session_state:
     uploaded_dataset = st.session_state["uploaded_dataset"]
     date_column = st.session_state["date_column"] # ensure its in correct form
-    sales_column = st.session_state["units_sold_column"]
+    units_sold_column = st.session_state["units_sold_column"]
     product_column = st.session_state["product_column"]
-    price_column = st.session_state["unit_price_column"]
+    unit_price_column = st.session_state["unit_price_column"]
+    uploaded_dataset = uploaded_dataset[[date_column, product_column, units_sold_column, unit_price_column]]
 
     # Pre Process
+
 
     # Fix Dates, Keep Consistent with region, remove extra details
     st.write("Processing Dates in the Correct Format")
 
-    # Identify Duplicates and drop
+    # Identify Duplicates and drop // maybe should keep incase identicial sales occur
     st.write("Handling Duplicates Rows")
     duplicates_exist = uploaded_dataset.duplicated(keep=False)  # `keep=False` marks all duplicates as True
     # Print the duplicate rows
@@ -44,9 +46,14 @@ if 'uploaded_dataset' in st.session_state:
     if uploaded_dataset[duplicates_exist].shape[0] == 0:
         st.success("Duplicates have been successfully removed")
 
+    # if non-numeric values found, make them null in dates, sales, quantity, price
+
     # Missing Values
+
     st.write("Handling Missing Values ")
     # if no dates -> out
+
+
 
     if uploaded_dataset.isnull().values.any():
         rows_with_missing_values = uploaded_dataset[uploaded_dataset.isnull().any(axis=1)]
@@ -57,67 +64,78 @@ if 'uploaded_dataset' in st.session_state:
         sales_imputer = SimpleImputer(strategy='median')
         price_imputer = SimpleImputer(strategy='mean')
 
-        product_imputer = SimpleImputer(strategy='most_frequent')
-        sales_imputer = SimpleImputer(strategy='median')
-        price_imputer = SimpleImputer(strategy='mean')
-
         rows_with_missing_values = uploaded_dataset[uploaded_dataset.isnull().any(axis=1)]
 
         # st.write(rows_with_missing_values.head())
-        uploaded_dataset.dropna(subset=date_column, inplace=True)
+        uploaded_dataset.dropna(subset=[date_column], inplace=True)
         ct = ColumnTransformer(
             [("product_imputer", product_imputer, [product_column]),
-             ("sales_imputer", sales_imputer, [sales_column]),
-             ("price_imputer", price_imputer, [price_column])],
+             ("sales_imputer", sales_imputer, [units_sold_column]),
+             ("price_imputer", price_imputer, [unit_price_column])],
             verbose_feature_names_out=False,
             remainder="passthrough")
 
         ct.set_output(transform="pandas")
         uploaded_dataset = ct.fit_transform(uploaded_dataset)
 
-        st.write(uploaded_dataset)
-
-        st.write("FINAL CHECK \n", uploaded_dataset[uploaded_dataset.isnull().any(axis=1)])
-
         if uploaded_dataset.isnull().values.any():
             st.warning("Missing Values Still Exists :/")
         else:
             st.success("Missing Values Processed and Cleaned")
-
-
     else:
         st.write("Data contains no missing or null values")
-
-    if st.session_state["selected_region"] == "UK":
-        for _ in stqdm(range(uploaded_dataset.shape[0])):
-            uploaded_dataset.at[_, date_column] = pd.to_datetime(uploaded_dataset[date_column][_], dayfirst=True).strftime("%d/%m/%Y")
+    # run check if format ok plot
+    if st.session_state["region"] == "UK":
+        uploaded_dataset[date_column] = pd.to_datetime(uploaded_dataset[date_column], errors="coerce")
+        # uploaded_dataset[date_column] = uploaded_dataset[date_column].dt.strftime("%d/%m/%Y")
+        st.write(uploaded_dataset.head())
         st.success("Dates have been successfully processed in the correct format")
 
-    elif st.session_state["selected_region"] == "USA":
-        for _ in stqdm(range(uploaded_dataset.shape[0])):
-            uploaded_dataset.at[_, date_column] = pd.to_datetime(uploaded_dataset[date_column][_], dayfirst=False).strftime("%m/%d/%Y")
-        st.success("Dates have been successfully processed in the correct format")
+    elif st.session_state["region"] == "USA":
+        uploaded_dataset[date_column] = pd.to_datetime(uploaded_dataset[date_column], errors="coerce")
+        # uploaded_dataset[date_column] = uploaded_dataset[date_column].dt.strftime("%d/%m/%Y")
+        st.write(uploaded_dataset.head())
+        st.success("Dates have been processed in the correct format")
+
     else:
         st.warning("Dates were not processed correctly :/")
+    st.session_state.uploaded_dataset = uploaded_dataset
+    # st.session_state.uploaded_dataset = uploaded_dataset.sort_values(date_column)
+    # uploaded_dataset = st.session_state.uploaded_dataset
+
+    # if st.session_state["selected_region"] == "UK":
+    #     for i in stqdm(range(uploaded_dataset.shape[0])):
+    #         uploaded_dataset.at[i, date_column] = pd.to_datetime(
+    #             uploaded_dataset.at[i, date_column], dayfirst=True, errors="coerce"
+    #         ).strftime("%d/%m/%Y")
+    #     st.success("Dates have been successfully processed in the correct format")
+    #     st.write(uploaded_dataset[date_column].head())
+    #
+    # elif st.session_state["selected_region"] == "USA":
+    #     for i in stqdm(range(uploaded_dataset.shape[0])):
+    #         uploaded_dataset.at[i, date_column] = pd.to_datetime(
+    #             uploaded_dataset.at[i, date_column], dayfirst=False, errors="coerce"
+    #         ).strftime("%m/%d/%Y")
+    #     st.success("Dates have been successfully processed in the correct format")
+    # else:
+    #     st.warning("Dates were not processed correctly :/")
 
 
     # Normalisation / Standardisation ?
     # Categorical Variables to Numerical e.g OHE or Label Encoding
-    # Column Specific Date to be Numerical, Product ID to be Numerical, Data to be seperated per product,
+    # Column Specific validation.. Date to be Numerical, Product ID to be Numerical, Data to be seperated per product,
     # Outliers/anomalies
-
-
 
     # visualise data
     visualise_button = st.button("Visualise Current Sales")
     if visualise_button:
         figure = go.Figure()
-        figure.add_trace(go.Scatter(x=uploaded_dataset[date_column],y=uploaded_dataset[sales_column]))
+        figure.add_trace(go.Scatter(x=uploaded_dataset[date_column],y=uploaded_dataset[units_sold_column]))
         figure.update_layout(
                     title_text="Current Sales Data",
                     xaxis=dict(rangeslider=dict(visible=True), type="date"),
                     xaxis_title=date_column,
-                    yaxis_title=sales_column,
+                    yaxis_title=units_sold_column,
                 )
         st.plotly_chart(figure)
 
